7c68fa57bd8e23122c7e48362b9f7691f96f8428
RexShack <rexshackuk@gmail.com>
2022-12-24 07:05:06 +0000

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'rsg-menu'
+
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css'
+}
+
+lua54 'yes'
