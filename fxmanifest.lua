6911bab8c06777668b5f73ca5ad476f6b2bbd0c7
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 14:14:37 +0100

    version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,16 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'Conversion of original qb-menu for RSGCore'
-version '0.0.1'
+description 'rsg-menu'
+version '1.0.0'
 
-client_script 'client/main.lua'
+client_scripts { 
+	'client/main.lua'
+}
+
+server_scripts { 
+	'server/server.lua'
+}
 
 ui_page 'html/index.html'
 
